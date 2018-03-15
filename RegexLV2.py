3818c5a1d19e314c42241e7bf9ff8647ba947048
He-XiangPeng <hxpeng1989@hotmail.com>
2018-03-15 22:01:54 +0800

    modified:   RegexLV2.py

--- RegexLV2.py
+++ RegexLV2.py
@@ -20,8 +20,10 @@ print(re.match('\w\w\w-\d\d\d', 'abc-xyz'))  # 不能匹配
 
 # 分组
 print('group ' + re.match('(\w\w\w)-(\d\d\d)', 'abc-123').group(0))  # 完整匹配
-print('group1' + ' ' + re.match('(\w\w\w)-(\d\d\d)', 'abc-123').group(1))  # 注意子组()
-print('group2' + ' ' + re.match('(\w\w\w)-(\d\d\d)', 'abc-123').group(2))  # 注意子组()
+print('group1' + ' ' + re.match('(\w\w\w)-(\d\d\d)',
+                                'abc-123').group(1))  # 注意子组()
+print('group2' + ' ' + re.match('(\w\w\w)-(\d\d\d)',
+                                'abc-123').group(2))  # 注意子组()
 print(re.match('(\w\w\w)-(\d\d\d)', 'abc-123').groups())  # 全部子组
 print(re.match('(a(b))', 'ab').groups())  # 两个子组
 
@@ -44,11 +46,11 @@ s = 'This and that.'
 list4 = re.findall(r'(th\w+) and (th\w+)', s, re.I)
 print(type(list4))
 for item in list4:
-	print (item)
+    print(item)
 # python3.6.4无法编译，2.7.10可编译
-# print(re.finditer(r'(th\w+) and (th\w+)', s, re.I).next().groups()) 
+# print(re.finditer(r'(th\w+) and (th\w+)', s, re.I).next().groups())
 for m in re.finditer(r'(th\w+) and (th\w+)', s, re.I):
-	print (m)
+    print(m)
 
 # 使用sub()和subn()搜索和替换
 print(re.sub('X', 'Mr. Smith', 'attn: X\n\nDear X, \n'))
@@ -58,6 +60,27 @@ print(re.subn('[ae]', 'X', 'abcedf'))
 
 print(re.split(':', 'str1:str2:str3'))
 DATA = ('Moutain View, CA 94040', 'Sunnyvale, CA', 'Los Altos, 94023',
-	'Cupertino, 95014', 'Palo Alto CA')
+        'Cupertino, 95014', 'Palo Alto CA')
 for datum in DATA:
-	print(re.split(', |(?= (?:\d{5}|[A-Z]{2})) ', datum))
+    print(re.split(', |(?= (?:\d{5}|[A-Z]{2})) ', datum))
+
+print(re.findall(r'(?i)yes', 'yes? Yes. YES!!'))  # 使用re.I/IGNORECASE
+# 使用re.I/IGNORECASE
+print(re.findall(r'(?i)th\w+', 'The quickest way is through this tunnel.'))
+print(re.findall(r'(?im)(^th[\w ]+)', """
+    This line is the first,
+    another line,
+    that line, it's the best
+    """))
+
+# re.S/DOTAALL, 该标记表示点号(.)能够用来表示\n符号(反之其通常用于表示除了\n之外的全部字符)
+print(re.findall(r'th.+', '''
+    This first line,
+    the second line,
+    the third line
+    '''))
+print(re.findall(r'(?s)th.+', '''
+    This first line,
+    the second line,
+    the third line
+    '''))
